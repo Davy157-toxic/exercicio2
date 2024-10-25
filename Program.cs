7c429d8728cb89bafa45405245a65255f5903b23
Davy <machadodavi3778@gmail.com>
2024-10-25 13:02:29 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+public class exercico2
+{
+   
+    public static void Main(string[] args)
+    {
+        double Raio = 10; 
+        double diametro = 2 * Raio; 
+        Console.WriteLine("O diametro e:" + diametro);
+
+
+
+
+
+
+
+    }
+}
